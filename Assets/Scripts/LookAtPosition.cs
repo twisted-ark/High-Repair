6bd3ca85bc51ed938431d852def5b85a1c19eb44
Sunrise Hirame <benhard.miettinen@gmail.com>
2020-01-31 22:51:30 +0200

    Some new shaders

---
+++ Assets/Scripts/LookAtPosition.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class LookAtPosition : MonoBehaviour
+{
+    [SerializeField] private float lookAtX;
+    [SerializeField] private float lookAtZ;
+
+    private void Update ()
+    {
+        var posY = transform.position.y;
+        transform.LookAt (new Vector3 (lookAtX, posY, lookAtZ));
+    }
+}
