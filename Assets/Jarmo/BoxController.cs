a41616e55fe0a29b012f8421470b4c239f43d441
JarmoKu <jarmo.august.kuutti@gmail.com>
2020-02-01 15:53:45 +0200

    Newest version

--- Assets/Jarmo/BoxController.cs
+++ Assets/Jarmo/BoxController.cs
@@ -1,24 +1,20 @@
-﻿using System;
-using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BoxController : MonoBehaviour
 {
-    [SerializeField] private Transform transform;
     private ControllableBox controllableBox;
     [SerializeField] private float speed;
     [SerializeField] private GameObject box;
+    private bool controlsDisabled;
 
-    [SerializeField] private Transform target;
+    [SerializeField] private Transform crane;
+    [SerializeField] private Transform spanwPoint;
 
     private Vector3 previousInput;
 
-    //public static event Action Dropped;
-    //public static event Action Landed;
-
     private void Awake ()
     {
-        controllableBox = Instantiate (box, target.position, Quaternion.identity).GetComponent<ControllableBox> ();
+        controllableBox = Instantiate (box, crane.position, Quaternion.identity).GetComponent<ControllableBox> ();
     }
 
     private void OnEnable ()
@@ -33,6 +29,9 @@ public class BoxController : MonoBehaviour
 
     void Update ()
     {
+        if (controlsDisabled)
+            return;
+
         Vector3 input = new Vector3 (Input.GetAxis ("Vertical"), 0f, -Input.GetAxis ("Horizontal"));
 
         input = Quaternion.Euler (0, -45, 0) * input;
@@ -43,32 +42,16 @@ public class BoxController : MonoBehaviour
         if (previousInput != Vector3.zero && input == Vector3.zero)
         {
             controllableBox.Drop ();
-            //enabled = false;
+            controlsDisabled = true;
         }
 
         previousInput = input;
     }
 
-    /*private void Drop()
-    {
-        Debug.Log("END");
-        controllableBox.rb.useGravity = true;
-        enabled = false;
-        //Dropped.Invoke ();
-        //StartCoroutine(SpawnTimer());
-    }*/
-
-    private IEnumerator SpawnTimer ()
-    {
-        yield return new WaitForSeconds (2);
-
-        Spawn ();
-    }
-
     private void Spawn ()
     {
-        //enabled = true;
-        target.position += new Vector3 (0, 1, 0);
-        controllableBox = Instantiate (box, target.position, Quaternion.identity).GetComponent<ControllableBox> ();
+        controlsDisabled = false;
+        crane.position += new Vector3 (0, 1, 0);
+        controllableBox = Instantiate (box, crane.position, Quaternion.identity).GetComponent<ControllableBox> ();
     }
 }
---
+++ Assets/Jarmo/Gravity.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float force;
+
+    private void FixedUpdate ()
+    {
+        rb.AddForce (Vector3.down * force);
+    }
+}
