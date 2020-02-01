06df1535f7fc2c1a76f03a4ad1b68bff3d776558
Iiro Kiema <37579193+Ibel44@users.noreply.github.com>
2020-02-01 22:02:28 +0200

    Camera shake script and prefab
    CameraShake prefab will launch camera shake every x seconds (default 10 seconds).

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class CameraShake : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera vcam;
+    [FormerlySerializedAs ("vcam2")] [SerializeField] private CinemachineVirtualCamera vcamWithShaking;
+    [SerializeField] private float timeBetweenQuakes = 10;
+    private float time;
+
+    private void Update ()
+    {
+        time += Time.deltaTime;
+        
+        if (time >= timeBetweenQuakes)
+        {
+            vcam.enabled = false;
+            vcamWithShaking.enabled = true;
+            StartCoroutine (nameof (StopCameraShakeAfterDelay));
+            time = 0;
+        }
+    }
+
+    IEnumerator StopCameraShakeAfterDelay ()
+    {
+        yield return new WaitForSeconds (2);
+        vcamWithShaking.enabled = false;
+        vcam.enabled = true;
+    }
+}
