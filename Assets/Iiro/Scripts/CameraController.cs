e996e7f013fe571ef3edd06b469430f4d11bf9b4
Iiro Kiema <37579193+Ibel44@users.noreply.github.com>
2020-02-01 14:11:56 +0200

    Smooth Camera
    The CameraController script should be added to Main Camera game object.

--- Assets/Iiro/Scripts/CameraController.cs
+++ Assets/Iiro/Scripts/CameraController.cs
@@ -3,28 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// This script should be added to Main Camera Game Object
+
 public class CameraController : MonoBehaviour
 {
-    private Vector3 position;
-    
-    private void Start ()
-    {
-        position = transform.position;
-    }
-    
+    [SerializeField] private float heightIncrease = 1.0f;
+    [SerializeField] private float deltaY = 0.05f;
+
     private void Update ()
     {
         if (Input.GetKeyUp (KeyCode.Space))
         {
-            Debug.Log ("Pressed Space Key");
-            RaiseCamera ();
+            Debug.Log ("Pressed Space");
+            RaiseCameraSmoothly ();
         }
     }
-    
-    private void RaiseCamera ()
+
+    public void RaiseCameraSmoothly ()
     {
-        Debug.Log ("Inside RaiseCamera function.");
-        position.y += 1;
-        Camera.main.transform.position = position;
+        StartCoroutine (Translate ());
+    }
+
+    IEnumerator Translate()
+    {
+        float t = 0.0f;
+        
+        while (t < heightIncrease)
+        {
+            transform.Translate (0, deltaY,0);
+            t += deltaY;
+            yield return null;
+        }
     }
 }
