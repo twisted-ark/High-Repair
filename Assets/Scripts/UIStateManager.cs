7445752d0f97073db77e160e926e257aeb4c4677
JarmoKu <jarmo.august.kuutti@gmail.com>
2020-02-02 16:45:35 +0200

    Update UIStateManager.cs

--- Assets/Scripts/UIStateManager.cs
+++ Assets/Scripts/UIStateManager.cs
@@ -7,7 +7,7 @@ public class UIStateManager : MonoBehaviour
     [SerializeField] private GameObject pauseCanvas;
     [SerializeField] private GameObject gameOverCanvas;
     [SerializeField] private GameObject runTimeCanvas;
-    
+
     public void Pause ()
     {
         Time.timeScale = 0;
@@ -17,7 +17,7 @@ public class UIStateManager : MonoBehaviour
 
     public void GameOver ()
     {
-        Time.timeScale = 0;
+        //Time.timeScale = 0;
         runTimeCanvas.GetComponent<Canvas> ().enabled = false;
         gameOverCanvas.GetComponent<Canvas> ().enabled = true;
     }
